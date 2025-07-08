ce2109211e164d112bef852136964ab49ac7cee7
CarolineSilva281 <carolinepereira2535@gmail.com>
2025-07-07 22:28:34 -0300

    Add files via upload

---
+++ exerciciolista.py
@@ -0,0 +1,59 @@
+#Exercício 1: Soma simples
+valor = float(input('digite o valor do produto'))
+desconto = valor * 0.10
+novoValor = valor - desconto
+print('novo valor')
+#Exercício 3: Divisão inteira e resto
+numero01 = float(input('Digite um numero para a divisão'))
+divisor = float(input('digite o numero para dividir'))
+divisaoInteira = numero01/divisor 
+restoDivisao= numero01%divisor
+print(f"a divisão do numero {numero01}/={divisor}= {divisaoInteira}")
+print(f"O Resto da divisão de {numero01} é {restoDivisao}")
+ #Exercício 4: Potência
+numero001 = int(input('Digite um numero'))
+expoente = int(input("Digite o expoente"))
+dois= numero001*expoente
+print(f"A potencia de {numero001} é {dois}")
+
+idade = int(input("Digite sua idade"))
+if (16<= idade <=65)
+print("pode entrar") 
+ else
+ print ("não pode entrar")
+
+#pode dirigir 
+podeDirigir = input("Você pode dirigir:")
+idade0003= int(input("Digite sua idade"))
+if (idade>=18 and podeDirigir== "sim"):
+print 
+
+#nota aluno
+nota = float(input("digite sua nota"))
+if (nota>=7):
+ print("aprovado")
+elif: (nota>= and <=6.9):
+ print("recuperação")
+
+ #maior numero
+ numeron= int(input("digite um numero:"))
+ numeron1= int(input("digite outro numero:"))
+ if (numeron>numeron1):
+  print(f"o maior numero é {numeron}"):
+ elif:
+(numeron1 == 0 and numeron1 ==0)
+else:
+  print(f"o maior numero é {numeron1}")
+
+
+#imc
+peso= float(int(input("digite seu peso em kg")))
+altura= float(int(input("digite sua altura"))
+ imc= peso/(altura**2)
+ if (imc<18):
+ print("esta abaixo do peso")
+ elif (imc>18.5 and imc == 24.9):
+ elif(imc>=25):
+ print("sobrepeso")
+ else 
+ print("obesidade")         
